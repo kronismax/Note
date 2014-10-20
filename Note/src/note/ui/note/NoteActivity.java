0987bb87285311acff6474d105bdd4b08d924db1
kronismax <lituchii.maksim@mail.ru>
2014-10-20 18:43:25 +0300

    AsyncLoader

--- Note/src/note/ui/note/NoteActivity.java
+++ Note/src/note/ui/note/NoteActivity.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import note.MyApplication;
 import note.api.API;
 import note.api.API.DeleteNoteResponse;
+import note.api.API.GetNoteResponse;
 import note.api.API.LogOutResponse;
 import note.api.API.NoteListResponse;
 import note.api.ApiException;
@@ -27,6 +28,7 @@ import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.Gravity;
@@ -226,9 +228,7 @@ public class NoteActivity extends FragmentActivity implements LoaderCallbacks<Cu
 		if (noteAdapter != null) {
 			//DBHelper db = new DBHelper(this);
 			//Cursor c = getContentResolver().query(MyContentProvider.URI_NOTE, myColumns, null, null, TableNote._ID);
-			//Log.d("cursor size", "" + c.getCount());
 			//noteAdapter.swapCursor(c);
-			getLoaderManager().getLoader(1).forceLoad();
 		}
 	}
 
@@ -278,10 +278,9 @@ public class NoteActivity extends FragmentActivity implements LoaderCallbacks<Cu
 
 							}
 							getContentResolver().bulkInsert(MyContentProvider.URI_NOTE, contentValues);
-							Cursor c = getContentResolver().query(MyContentProvider.URI_NOTE, myColumns, null, null, TableNote._ID);
+							//Cursor c = getContentResolver().query(MyContentProvider.URI_NOTE, myColumns, null, null, TableNote._ID);
 							//noteAdapter.swapCursor(c);
-							updateNoteAdapter();
-							getLoaderManager().getLoader(MyContentProvider.URI_NOTE, myColumns, null, null, TableNote._ID);
+							//updateNoteAdapter();
 						}
 						break;
 					case 1:
@@ -391,20 +390,120 @@ public class NoteActivity extends FragmentActivity implements LoaderCallbacks<Cu
 		noteAdapter.swapCursor(null);
 	}
 
-//	static class MyCursorLoader extends CursorLoader {
-//
-//		Cursor	c;
-//
-//		public MyCursorLoader(Context context,Cursor c) {
-//			super(context);
-//			this.c = c;
-//		}
-//
-//		@Override
-//		public Cursor loadInBackground(){
-//			return c;
-//		}
-//
-//	}
+	public class NoteListAsyncTaskLoader extends AsyncTaskLoader<GetNoteResponse> {
+
+		  // We hold a reference to the Loader’s data here.
+		  private GetNoteResponse mData;
+		  
+		  private String ID;
+		  
+		  public NoteListAsyncTaskLoader(Context ctx, String ID) {
+		    // Loaders may be used across multiple Activitys (assuming they aren't
+		    // bound to the LoaderManager), so NEVER hold a reference to the context
+		    // directly. Doing so will cause you to leak an entire Activity's context.
+		    // The superclass constructor will store a reference to the Application
+		    // Context instead, and can be retrieved with a call to getContext().
+		    super(ctx);
+		    this.ID = ID;
+		  }
+
+		  /****************************************************/
+		  /** (1) A task that performs the asynchronous load **/
+		  /****************************************************/
+
+		  @Override
+		  public GetNoteResponse loadInBackground() {
+		    // This method is called on a background thread and should generate a
+		    // new set of data to be delivered back to the client.
+			  GetNoteResponse data = API.getNote(ID, mLastLoadCompleteTime);
+
+		    return data;
+		  }
+
+		  /********************************************************/
+		  /** (2) Deliver the results to the registered listener **/
+		  /********************************************************/
+
+		  @Override
+		  public void deliverResult(List<SampleItem> data) {
+		    if (isReset()) {
+		      // The Loader has been reset; ignore the result and invalidate the data.
+		      releaseResources(data);
+		      return;
+		    }
+
+		    // Hold a reference to the old data so it doesn't get garbage collected.
+		    // We must protect it until the new data has been delivered.
+		    List<SampleItem> oldData = mData;
+		    mData = data;
+
+		    if (isStarted()) {
+		      // If the Loader is in a started state, deliver the results to the
+		      // client. The superclass method does this for us.
+		      super.deliverResult(data);
+		    }
+
+		    // Invalidate the old data as we don't need it any more.
+		    if (oldData != null && oldData != data) {
+		      releaseResources(oldData);
+		    }
+		  }
+
+		  /*********************************************************/
+		  /** (3) Implement the Loader’s state-dependent behavior **/
+		  /*********************************************************/
+
+		  @Override
+		  protected void onStartLoading() {
+		    if (mData != null) {
+		      // Deliver any previously loaded data immediately.
+		      deliverResult(mData);
+		    }
+
+		    if (takeContentChanged() || mData == null) {
+		      // When the observer detects a change, it should call onContentChanged()
+		      // on the Loader, which will cause the next call to takeContentChanged()
+		      // to return true. If this is ever the case (or if the current data is
+		      // null), we force a new load.
+		      forceLoad();
+		    }
+		  }
+
+		  @Override
+		  protected void onStopLoading() {
+		    // The Loader is in a stopped state, so we should attempt to cancel the 
+		    // current load (if there is one).
+		    cancelLoad();
+		  }
+
+		  @Override
+		  protected void onReset() {
+		    // Ensure the loader has been stopped.
+		    onStopLoading();
+
+		    // At this point we can release the resources associated with 'mData'.
+		    if (mData != null) {
+		      releaseResources(mData);
+		      mData = null;
+		    }
+		  }
+
+		  @Override
+		  public void onCanceled(List<SampleItem> data) {
+		    // Attempt to cancel the current asynchronous load.
+		    super.onCanceled(data);
+
+		    // The load has been canceled, so we should release the resources
+		    // associated with 'data'.
+		    releaseResources(data);
+		  }
+
+		  private void releaseResources(List<SampleItem> data) {
+		    // For a simple List, there is nothing to do. For something like a Cursor, we 
+		    // would close it in this method. All resources associated with the Loader
+		    // should be released here.
+		  }
+
+
 
 }
